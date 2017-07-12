1a37d08c6c2bdb1bd208a68fdf26f73f876af76f
Luiz Maffeis <luiz.maffeis@gmail.com>
2017-07-12 08:49:14 -0300

    first

---
+++ src/main/java/de/com/ginmon_core/App.java
@@ -0,0 +1,13 @@
+package de.com.ginmon_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
